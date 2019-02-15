142ceb9c88e50230148953f6162198ef93f3947e
iscovici lucas <letmfrance@gmail.com>
2019-02-15 18:32:32 +0000

    first

---
+++ R/icor.R
@@ -0,0 +1,208 @@
+#' @import data.table
+#' @import tibble
+#' @import Hmisc
+#' @import Matrix
+#" @export
+icor = function(data_,verbose=F,pValueMin_=0.05,seuil_=0.3,graph=T,normul=F,signeOK=T,pcorOK=T,criticalSup=T){
+    data=data_
+    if(!is.matrix(data)) data=as.matrix(data)
+    
+    
+    suppressMessages(library("data.table"))
+    #source("./pcor.R")
+    suppressMessages(library("tibble"))
+   suppressMessages( library("Hmisc"))
+    suppressMessages(library("Matrix"))
+    seuil=seuil_
+    seuilPcor=seuil
+    p_valueMin=pValueMin_
+    
+    #Cor total
+    dataCor_=rcorr(data)
+    dataCor=dataCor_$r
+    dataCorP=dataCor_$P
+     if(normul){
+         #p_valueMin=1
+          #seuil=0
+        #signeOK=F
+         pcorOK=F
+         #criticalSup=F
+     }
+    if(criticalSup){
+        if (seuil_!=0.3) print("WHEN criticalSup, is set, seuil_, is not considerer")
+      seuil = icor.critical.r(nrow(data)) 
+      seuilPcor = icor.critical.r(nrow(data)-1) 
+        cat(paste("seuil: ",seuil,"\nseuilPcor",seuilPcor))
+      }
+    
+    dataColsName = colnames(data)
+    #On enleve les pas signif
+    dataCor[upper.tri(dataCor,diag = T) | (dataCorP > p_valueMin)]=NA
+   
+    dataCorSum=Matrix::summary(as(dataCor,"dgCMatrix"))
+    data.table::setDT(dataCorSum)
+    print(class(dataCorSum))
+    #On garde les cor >= seuil
+    dataCorT= dataCorSum[!is.na(x) & abs(x)>=seuil,]
+    #print("ici")
+    nbCol = ncol(data)
+    km=1:nbCol
+    nbRow=nrow(data)
+    nbCorT=nrow(dataCorT)
+    signifRel=data.frame()
+    PASsignifRel=data.frame()
+    keys=rep(NA,nbCol)
+    keysName=rep(NA,nbCol)
+    keysi=0
+    
+    #on loop les cor total respectant le seuil et p_value....
+    #on regarde si les corPartielle avec chaque var est du meme signe, et sup > seuilPcor, et p_value bien
+    for(i in 1:nbCorT){
+        elem=dataCorT[i,]
+        elemX=elem[,3]
+        elemCol=as.integer(elem[,2])
+        elemRow=as.integer(elem[,1])
+        kmI=km[km!=elemRow & km!=elemCol]
+        pb=F
+        pcors=list()
+        if(verbose)print(elem)
+        for(j in kmI){
+            if(verbose)print(j)    
+            tryCatch({ pcorIJ=pcor.test(data[,elemCol],data[,elemRow],data[,j]) },
+                         error = function(err) {
+                                print("error")
+                                print(err)
+                                print(list(data[,elemCol],data[,elemRow],data[,j]))
+                         }
+                    )
+            pcorIJVal=pcorIJ$estimate
+            pcorIJP=pcorIJ$p
+            pcors=c(pcors,list(pcorIJ))
+            if(verbose)print(pcorIJ)
+            #check p_value, signe, seuil
+            if((pcorOK && (pcorIJP >= p_valueMin || (signeOK && pcorIJVal*elemX < 0) || abs(pcorIJVal) < seuilPcor))){
+                if(verbose)print("fini")
+                pb=T
+                PASsignifRel=rbind(PASsignifRel,
+                                      data.frame(i = dataColsName[elemCol],
+                                        j = dataColsName[elemRow],
+                                       cor=as.numeric(elemX),
+                                       cor_pVal=dataCorP[dataColsName[elemCol],dataColsName[elemRow]],
+                                       pcor=paste(unlist(lapply(pcors,function(x)paste(x$estimate,"_",x$p))),collapse="|")
+                                                 ))
+                break
+            }
+        }
+        
+        if(!pb){
+            if(verbose)print(paste(i,"ok"))
+            i_=keys[elemCol]
+            if(is.na(i_)){
+                keysi=keysi+1
+                i_=keysi
+                keys[elemCol]=i_
+                keysName[i_]=dataColsName[elemCol]
+            }
+            j_=keys[elemRow]
+            if(is.na(j_)){
+                keysi=keysi+1
+                j_=keysi
+                keys[elemRow]=j_
+                keysName[j_]=dataColsName[elemRow]
+            }
+            signifRel=rbind(signifRel,
+                            data.frame(i = dataColsName[elemCol],
+                                       i_=i_,
+                                       j_=j_,
+                                       j = dataColsName[elemRow],
+                                       cor=as.numeric(elemX),
+                                       cor_pVal=dataCorP[dataColsName[elemCol],dataColsName[elemRow]],
+                                       pcor=pcors
+                                       )
+                           )
+        }
+    }
+    if(graph){
+        graphO=icor.graph(signifRel,keysName)
+    }
+    invisible(list(graph=graphO,pasSignif=PASsignifRel,signif=signifRel,dataCorT=dataCorT,dataCorP=dataCorP,dataCor=dataCor,dataCor_=dataCor_$r))
+    
+}
+icor.graph = function(dataSet,names){
+    library("visNetwork")
+        signif=dataSet
+        nodes <- data.frame(id=1:max(signif[,c("i_","j_")]),label=names[!is.na(names)])
+    
+        edges <- tibble(from=integer(),to=integer(),dashes=logical(),label=numeric(),width=numeric())
+        for(i in 1:nrow(signif)){
+            elem=signif[i,]
+            #print()
+             edges=add_row(edges,from=c(elem[,"i_"]),to=c(elem[,"j_"]),dashes=c(elem[,"cor"]<0),label=round(elem[,"cor"],2),width=exp(abs(elem[,"cor"])*2.5))
+            
+        }
+        return(visNetwork(nodes,edges,width = "100%"))
+}
+#get minimun corr pour etre signif en fonction de n et alpha
+icor.critical.r <- function( n, alpha = .05 ) {
+  df <- n - 2 # car estimationde muX et muY
+  critical.t <- qt( alpha/2, df, lower.tail = F ) # pour avoir le t, correpondant a (a gauche et a droite, alpha/2)
+  critical.r <-  icor.studentToCorr(critical.t,df) # pour passer de t à r # critical.t / sqrt( (critical.t^2) + df )
+  return( critical.r )
+}
+
+#table to html table                                     
+icor.dfToHTML = function(df){
+    suppressMessages(library("googleVis"))
+    IRdisplay::display_html(icor.repr_html.gvis(gvisTable(df,options=list(page='enable'))))
+}
+icor.repr_text.gvis <- function(obj, ...) 'gvis cannot be represented in plain text (need html)'
+
+icor.repr_html.gvis <- function(obj, ...){
+    if (!requireNamespace('googleVis', quietly = TRUE))
+        stop('repr_html.gvis called without loadable googleVis')
+
+    htmlfile <- tempfile(fileext = '.html')
+    on.exit(unlink(htmlfile))
+
+    print(obj, tag='chart', file=htmlfile)
+    readChar(htmlfile, file.info(htmlfile)$size)
+}
+icor.repr_latex.gvis <- function(obj, ...) NULL
+icor.repr_markdown.gvis <- function(obj, ...) NULL
+                                                  
+icor.studentToCorr = function(t,dll){
+    t / sqrt( (t^2) + dll )
+}
+icor.corrToStudent = function(r,dll){
+    r * sqrt(dll/(1-(r^2)))
+}
+                                                  blib=base::library
+library = function(pkg){
+    if(!require(pkg,character.only = TRUE)){
+        install.packages(pkg)
+    }
+    tg(blib(pkg,character.only = TRUE, quietly = TRUE))
+}
+tg = function(f){
+    suppressMessages(suppressPackageStartupMessages(suppressWarnings(f)))
+}
+Lib <- function(){
+    x <- list()
+    class(x) <- "Lib"
+    return(x)
+}
+print.Lib <- function(e1,e2=NULL) {
+  return("OK")
+}
+
+is.Lib <- function(obj) {
+  return(class(obj) == "Lib")
+}
+
+`+.Lib` <- function(e1, e2){
+    print(e1)
+    tg(library(e2))
+    return(Lib())
+     #NextMethod(e1,e2)
+}
+lib = Lib()
---
+++ R/pcor.R
@@ -0,0 +1,304 @@
+# partial correlation
+pcor.test <- function(x,y,z,use="mat",method="p",na.rm=T,pvalueFun=pnorm){
+	# The partial correlation coefficient between x and y given z
+	#
+	# pcor.test is free and comes with ABSOLUTELY NO WARRANTY.
+	#
+	# x and y should be vectors
+	#
+	# z can be either a vector or a matrix
+	#
+	# use: There are two methods to calculate the partial correlation coefficient.
+	#	 One is by using variance-covariance matrix ("mat") and the other is by using recursive formula ("rec").
+	#	 Default is "mat".
+	#
+	# method: There are three ways to calculate the correlation coefficient, 
+	#	    which are Pearson's ("p"), Spearman's ("s"), and Kendall's ("k") methods.
+	# 	    The last two methods which are Spearman's and Kendall's coefficient are based on the non-parametric analysis.
+	#	    Default is "p".
+	#
+	# na.rm: If na.rm is T, then all the missing samples are deleted from the whole dataset, which is (x,y,z).
+	#        If not, the missing samples will be removed just when the correlation coefficient is calculated.
+	#	   However, the number of samples for the p-value is the number of samples after removing 
+	#	   all the missing samples from the whole dataset.
+	#	   Default is "T".
+
+	x <- c(x)
+	y <- c(y)
+	z <- as.data.frame(z)
+
+	if(use == "mat"){
+		p.use <- "Var-Cov matrix"
+		pcor = pcor.mat(x,y,z,method=method,na.rm=na.rm)
+	}else if(use == "rec"){
+		p.use <- "Recursive formula"
+		pcor = pcor.rec(x,y,z,method=method,na.rm=na.rm)
+	}else{
+		stop("\'use\' should be either \"rec\" or \"mat\"!\n")
+	}
+
+	# print the method
+	if(gregexpr("p",method)[[1]][1] == 1){
+		p.method <- "Pearson"
+	}else if(gregexpr("s",method)[[1]][1] == 1){
+		p.method <- "Spearman"
+	}else if(gregexpr("k",method)[[1]][1] == 1){
+		p.method <- "Kendall"
+	}else{
+		stop("\'method\' should be \"pearson\" or \"spearman\" or \"kendall\"!\n")
+	}
+
+	# sample number
+	n <- dim(na.omit(data.frame(x,y,z)))[1]
+	
+	# given variables' number
+	gn <- dim(z)[2]
+
+	# p-value
+    pvalueFunc = pvalueFun
+	if(p.method == "Kendall"){
+		statistic <- pcor/sqrt(2*(2*(n-gn)+5)/(9*(n-gn)*(n-1-gn)))
+		p.value <- 2*pvalueFunc(-abs(statistic))
+
+	}else{
+		statistic <- pcor*sqrt((n-2-gn)/(1-pcor^2))
+  		p.value <- 2*pvalueFunc(-abs(statistic))
+	}
+
+	data.frame(estimate=pcor,p.value=p.value,statistic=statistic,n=n,gn=gn,Method=p.method,Use=p.use)
+}			
+
+# By using var-cov matrix
+pcor.mat <- function(x,y,z,method="p",na.rm=T){
+
+	x <- c(x)
+	y <- c(y)
+	z <- as.data.frame(z)
+
+	if(dim(z)[2] == 0){
+		stop("There should be given data\n")
+	}
+
+	data <- data.frame(x,y,z)
+
+	if(na.rm == T){
+		data = na.omit(data)
+	}
+
+	xdata <- na.omit(data.frame(data[,c(1,2)]))
+	Sxx <- cov(xdata,xdata,m=method)
+
+	xzdata <- na.omit(data)
+	xdata <- data.frame(xzdata[,c(1,2)])
+	zdata <- data.frame(xzdata[,-c(1,2)])
+	Sxz <- cov(xdata,zdata,m=method)
+
+	zdata <- na.omit(data.frame(data[,-c(1,2)]))
+	Szz <- cov(zdata,zdata,m=method)
+
+	# is Szz positive definite?
+	zz.ev <- eigen(Szz)$values
+	if(min(zz.ev)[1]<0){
+		stop("\'Szz\' is not positive definite!\n")
+	}
+
+	# partial correlation
+	Sxx.z <- Sxx - Sxz %*% solve(Szz) %*% t(Sxz)
+	
+	rxx.z <- cov2cor(Sxx.z)[1,2]
+
+	rxx.z
+}
+
+# By using recursive formula
+pcor.rec <- function(x,y,z,method="p",na.rm=T){
+	# 
+
+	x <- c(x)
+	y <- c(y)
+	z <- as.data.frame(z)
+
+	if(dim(z)[2] == 0){
+		stop("There should be given data\n")
+	}
+
+	data <- data.frame(x,y,z)
+
+	if(na.rm == T){
+		data = na.omit(data)
+	}
+
+	# recursive formula
+	if(dim(z)[2] == 1){
+		tdata <- na.omit(data.frame(data[,1],data[,2]))
+		rxy <- cor(tdata[,1],tdata[,2],m=method)
+
+		tdata <- na.omit(data.frame(data[,1],data[,-c(1,2)]))
+		rxz <- cor(tdata[,1],tdata[,2],m=method)
+
+		tdata <- na.omit(data.frame(data[,2],data[,-c(1,2)]))
+		ryz <- cor(tdata[,1],tdata[,2],m=method)
+
+		rxy.z <- (rxy - rxz*ryz)/( sqrt(1-rxz^2)*sqrt(1-ryz^2) )
+		
+		return(rxy.z)
+	}else{
+		x <- c(data[,1])
+		y <- c(data[,2])
+		z0 <- c(data[,3])
+		zc <- as.data.frame(data[,-c(1,2,3)])
+
+		rxy.zc <- pcor.rec(x,y,zc,method=method,na.rm=na.rm)
+		rxz0.zc <- pcor.rec(x,z0,zc,method=method,na.rm=na.rm)
+		ryz0.zc <- pcor.rec(y,z0,zc,method=method,na.rm=na.rm)
+		
+		rxy.z <- (rxy.zc - rxz0.zc*ryz0.zc)/( sqrt(1-rxz0.zc^2)*sqrt(1-ryz0.zc^2) )
+		return(rxy.z)
+	}			
+}
+pcor_ <- function(x, method = c("pearson", "kendall", "spearman"))
+{
+  # correlation method
+  method <- match.arg(method)
+  
+  # check the data
+  if (is.data.frame(x)) 
+    x <- as.matrix(x)
+  if (!is.matrix(x)) 
+    stop("supply a matrix-like 'x'")
+  if (!(is.numeric(x) || is.logical(x))) 
+    stop("'x' must be numeric")
+  stopifnot(is.atomic(x))
+  
+  # sample number
+  n <- dim(x)[1]
+  
+  # given variables' number
+  gp <- dim(x)[2]-2
+  
+  # covariance matrix
+  cvx <- cov(x,method=method)
+  
+  # inverse covariance matrix
+  icvx <- solve(cvx)
+  
+  # partial correlation
+  pcor <- -cov2cor(icvx)
+  diag(pcor) <- 1
+  
+  # p-value
+  if(method == "kendall"){
+    statistic <- pcor/sqrt(2*(2*(n-gp)+5)/(9*(n-gp)*(n-1-gp)))
+    p.value <- 2*pnorm(-abs(statistic))
+    
+  }else{r=
+    statistic <- pcor*sqrt((n-2-gp)/(1-pcor^2))
+    p.value <- 2*pt(-abs(statistic),n-2-gp)
+  }
+  
+  diag(statistic) <- 0
+  diag(p.value) <- 0
+  
+  list(estimate=pcor,p.value=p.value,statistic=statistic,n=n,gp=gp,method=method)
+}
+
+# semi-partial (part) correlation
+spcor <- function(x, method = c("pearson", "kendall", "spearman"))
+{
+  # correlation method
+  method <- match.arg(method)
+  
+  # check the data
+  if (is.data.frame(x)) 
+    x <- as.matrix(x)
+  if (!is.matrix(x)) 
+    stop("supply a matrix-like 'x'")
+  if (!(is.numeric(x) || is.logical(x))) 
+    stop("'x' must be numeric")
+  stopifnot(is.atomic(x))
+  
+  # sample number
+  n <- dim(x)[1]
+  
+  # given variables' number
+  gp <- dim(x)[2]-2
+  
+  # covariance matrix
+  cvx <- cov(x,method=method)
+  
+  # inverse covariance matrix
+  icvx <- solve(cvx)
+  
+  # semi-partial correaltion
+  spcor <- -cov2cor(icvx)/sqrt(diag(cvx))/sqrt(abs(diag(icvx)-t(t(icvx^2)/diag(icvx))))
+  diag(spcor) <- 1
+  
+  # p-value
+  if(method == "kendall"){
+    statistic <- spcor/sqrt(2*(2*(n-gp)+5)/(9*(n-gp)*(n-1-gp)))
+    p.value <- 2*pnorm(-abs(statistic))
+    
+  }else{
+    statistic <- spcor*sqrt((n-2-gp)/(1-spcor^2))
+    p.value <- 2*pnorm(-abs(statistic))
+  }
+  
+  diag(statistic) <- 0
+  diag(p.value) <- 0
+  
+  list(estimate=spcor,p.value=p.value,statistic=statistic,n=n,gp=gp,method=method)
+}
+
+# pairwise partial correlation
+pcor.test_ <- function(x,y,z,method=c("pearson", "kendall", "spearman"))
+{
+  # The partial correlation coefficient between x and y given z
+  #
+  # pcor.test is free and comes with ABSOLUTELY NO WARRANTY.
+  #
+  # x and y should be vectors
+  #
+  # z can be either a vector or a matrix
+  
+  # correlation method
+  method <- match.arg(method)
+  
+  x <- c(x)
+  y <- c(y)
+  z <- as.data.frame(z)
+  
+  # merge into a matrix
+  xyz <- data.frame(x,y,z)
+  
+  # partial correlation
+  pcor = pcor_(xyz,method=method)
+  
+  data.frame(estimate=pcor$est[1,2],p.value=pcor$p.value[1,2],statistic=pcor$statistic[1,2],n=pcor$n,gp=pcor$gp,Method=method)
+}	
+
+# pairwise semi-partial (part) correlation
+spcor.test <- function(x,y,z,method=c("pearson", "kendall", "spearman"))
+{
+  # The semi-partial (part) correlation coefficient between x and y given z
+  #
+  # spcor.test is free and comes with ABSOLUTELY NO WARRANTY.
+  #
+  # x and y should be vectors
+  #
+  # z can be either a vector or a matrix
+  
+  # correlation method
+  method <- match.arg(method)
+  
+  x <- c(x)
+  y <- c(y)
+  z <- as.data.frame(z)
+  
+  # merge into a matrix
+  xyz <- data.frame(x,y,z)
+  
+  # semi-partial (part) correlation
+  spcor = spcor(xyz,method=method)
+  
+  data.frame(estimate=spcor$est[1,2],p.value=spcor$p.value[1,2],statistic=spcor$statistic[1,2],n=spcor$n,gp=spcor$gp,Method=method)
+}
