8358b9301f54254981d240c8c7ebde1ba690a0a8
luluperet <luluisco@yahoo.fr>
2019-03-26 17:12:54 +0100

    Update icor.R

--- R/icor.R
+++ R/icor.R
@@ -810,9 +810,9 @@ Aleatoire <- R6Class("Aleatoire",
     }
     l_ = function(...,env=NULL,parent=NULL){
       calls  <- match.call()
-      parent <- if(is.null(parent)) parent.frame() else parent
+      parent <- if(is.null(parent)){ parent.frame() }else{ parent}
       
-      env    <- if(is.null(env)) new.env(parent = parent) else env
+      env    <- if(is.null(env)){ new.env(parent = parent) }else {env}
       
       if(length(calls)<2){
         return(list())
