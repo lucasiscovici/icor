43553178dac052bf04340b7d5ddd9a1b06273465
luluperet <luluisco@yahoo.fr>
2019-03-26 17:18:53 +0100

    Update icor.R

--- R/icor.R
+++ R/icor.R
@@ -751,8 +751,8 @@ Aleatoire <- R6Class("Aleatoire",
       dfn=d[[3L]]
       if (is_funexpr(dfn)){
        pls=as.list(d)
-       splitArgs(pls[[3L]],env,parent)
-       p=l_(dfn,env=env,parent=parent)
+       p=splitArgs(pls[[3L]],env,parent)
+       #p=l_(dfn,env=env,parent=parent)
        }else{
         p=splitArgsl_(dfn,env,parent)
        }
