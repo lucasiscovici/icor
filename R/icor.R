2d81f54ac324d890d1f1dd35685cedc2bc6f082a
luluperet <luluisco@yahoo.fr>
2019-04-21 20:19:38 +0200

    Update icor.R

--- R/icor.R
+++ R/icor.R
@@ -2343,6 +2343,24 @@ updateIcor_special = function(){# update icor from github, (bug sometimes with u
             hidePlot({
     plot_blank=as_ggplot(grid::grid.rect(gp=grid::gpar(col="white")))
 })
+           
+           detachAllPackages <- function(keep = NULL, keep.basic = TRUE) {
+	# function for detaching all attached packages (except basic ones)
+	basic.packages <- c("package:stats","package:graphics","package:grDevices",
+											"package:utils","package:datasets","package:methods",
+											"package:base")
+	package.list <- search()[ifelse(unlist(gregexpr("package:", search())) == 1,
+																	TRUE, FALSE)]
+	if (!is.null(keep)){
+		package.list <- setdiff(package.list, paste("package", keep, sep = ":"))
+	}
+	if (keep.basic){
+		package.list <- setdiff(package.list, basic.packages)
+	}
+	if (length(package.list) > 0) {
+		for (package in package.list) detach(package, character.only = TRUE,unload = T,force = T)
+	}
+}
            #al = Aleatoire$new()
 #al$generer()
 #al$generer(max=10)
